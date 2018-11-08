0d1df1b204a4572c8c2cbe83de30fc76d6686052
Fatma Nur Bulacak (SahaBT) <ExtNurB@d-teknoloji.com.tr>
2018-11-08 10:30:13 +0300

    ilk kayit

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulke birinciUlke = new ulke();
+            birinciUlke.UlkeAdi = "Türkiye";
+            birinciUlke.Baskent = "Ankara";
+            birinciUlke.ParaBirimi = "TL";
+            birinciUlke.Nufus = 70000000;
+            birinciUlke.Yazdir();
+
+
+
+            ulke ikinciUlke = new ulke();
+            ikinciUlke.UlkeAdi = "Almanya";
+            ikinciUlke.Baskent = "Berlin";
+            ikinciUlke.Nufus = 80000000;
+            ikinciUlke.ParaBirimi = "Euro";
+            ikinciUlke.Yazdir();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Encapsulation/ulke.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Kapsülleme (Encapsulation) kavramı bir nesnenin bazı özellik ve işlevlerini başka sınıflardan ve nesnelerden saklamaktır.
+/// Private erişim belirteci sayesinde bu şekilde tanımlanan bir field’ i başka sınıflardan gizlemiş oluruz.
+/// Ayrıca bu field başka sınıflarda kullanılamaz.
+/// Kapsülleme (Encapsulation) sayesinde nesneler bilinçsiz kullanımdan korunmuş olur.
+/// Fakat bazı durumlarda private field’ lara erişmemiz ve özelliklerini kullanmamız gerekebilir. 
+/// Bu durumda Property kavramı devreye girer. Property bir field’ in değerini geri döndürmeye (Get) ve değerini ayarlamaya (Set) olanak sağlar.
+/// </summary>
+namespace Encapsulation
+{
+    class ulke
+    {
+       private string ulkeAdi; 
+
+        public string UlkeAdi
+        {
+            get
+            {
+                return ulkeAdi;
+            }
+            set
+            {
+                ulkeAdi = value;
+            }
+        }
+
+       private string baskent;
+        public string Baskent
+        {
+            get
+            {
+                return baskent;
+            }
+            set
+            {
+                baskent = value;
+            }
+        }
+
+        private int nufus;
+
+        public int Nufus
+        {
+            get
+            {
+                return nufus;
+            }
+            set
+            {
+                nufus = value;
+            }
+        }
+
+       private string paraBirimi;
+        public string ParaBirimi
+        {
+            get
+            {
+                return paraBirimi;
+            }
+            set
+            {
+                paraBirimi = value;
+            }
+        }
+
+        public void Yazdir()
+        {
+            Console.WriteLine("Ülke adı: " + ulkeAdi);
+            Console.WriteLine("Başkenti: " + baskent);
+            Console.WriteLine("Nüfusu: " + nufus);
+            Console.WriteLine("Para Birimi: " + paraBirimi);
+            Console.WriteLine("////////////////////////////");
+
+        }
+
+    }
+}
