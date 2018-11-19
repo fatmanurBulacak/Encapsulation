e72b72a2fc125998b60c90c9bf284b2c93183c62
Fatma Nur <30408581+fatmanurBulacak@users.noreply.github.com>
2018-11-19 10:12:01 +0300

    Update ulke.cs

--- Encapsulation/ulke.cs
+++ Encapsulation/ulke.cs
@@ -5,12 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 /// <summary>
 /// https://www.ahmetcansever.com/programlama/c-class-encapsulation-kapsulleme/ Teşekkürler
-/// Kapsülleme (Encapsulation) kavramı bir nesnenin bazı özellik ve işlevlerini başka sınıflardan ve nesnelerden saklamaktır.
-/// Private erişim belirteci sayesinde bu şekilde tanımlanan bir field’ i başka sınıflardan gizlemiş oluruz.
-/// Ayrıca bu field başka sınıflarda kullanılamaz.
-/// Kapsülleme (Encapsulation) sayesinde nesneler bilinçsiz kullanımdan korunmuş olur.
-/// Fakat bazı durumlarda private field’ lara erişmemiz ve özelliklerini kullanmamız gerekebilir. 
-/// Bu durumda Property kavramı devreye girer. Property bir field’ in değerini geri döndürmeye (Get) ve değerini ayarlamaya (Set) olanak sağlar.
 /// </summary>
 namespace Encapsulation
 {
